f9d882b7aace387b499a8f514f0a1e81757bcac2
pmall <pierre.mallinjoud@gmail.com>
2017-06-20 10:37:29 +0200

    add a register function method signature

--- src/EngineInterface.php
+++ src/EngineInterface.php
@@ -5,6 +5,15 @@ namespace Ellipse\Contracts\Templating;
 interface EngineInterface
 {
     /**
+     * Register the given callable as a function in the template engine.
+     *
+     * @param string    $name
+     * @param callable  $cb
+     * @return void
+     */
+    public function registerFunction(string $name, callable $cb): void;
+
+    /**
      * Return a html string from a template file and a list of values.
      *
      * @param string    $file
